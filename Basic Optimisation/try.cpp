3238a2e11237a7d474d4a9862087b46866c01e6b
Yash Chaudhari <chaudhariyash10@gmail.com>
2020-12-23 12:01:30 +0530

    Basic Optimisation Problems

--- Basic Optimisation/try.cpp
+++ Basic Optimisation/try.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main()
 {
-    
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            cout << "hii" << endl;
+            break;
+        }
+    }
 }
