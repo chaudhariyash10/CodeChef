b58dca38a99bf6cb9b839206f83647878a10f853
Yash Chaudhari <chaudhariyash10@gmail.com>
2021-01-22 10:19:57 +0530

    Basic DP problems

---
+++ Basic Dynamic Programming/Chef And Frogs.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, p, k;
+    cin >> n >> k >> p;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        
+    }
+
+    sort(sorted, sorted + n);
+
+    bool dp[n + 1][n + 1];
+}
